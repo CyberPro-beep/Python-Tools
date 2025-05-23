664f0d998969b2566723940b3708eec82c4548dc
CyberPro-beep <prince.adoleyine.pro@gmail.com>
2025-05-23 22:10:03 +0000

    added input prompt

--- scanner.py
+++ scanner.py
@@ -5,8 +5,8 @@ import socket
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.settimeout(1)
 
-host = "192.168.100.68"
-port = 40
+host = input("[*] Enter the IP address of system you want to scan it's port: ")
+port = int(input("[*] Enter the port to scan"))
 
 def port_scanner(port):
     if connection.connect_ex((host, port)):
